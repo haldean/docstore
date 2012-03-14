4e0fc369c935f909cccaab521e359dc0f88dd9f2
Will Brown <will.h.brown@gmail.com>
2012-03-14 11:03:34 -0400

    Fix bug with trailing slashes, update title on load

--- js/docstore.js
+++ js/docstore.js
@@ -2,10 +2,11 @@ var homepage = 'http://haldean.org'
 
 $.domReady(function() {
   var page = document.location.search.substring(1)
-  if (page[-1] == '/') page = page.substring(0, page.length - 1)
+  if (page[page.length - 1] == '/') page = page.substring(0, page.length - 1)
   if (!page) {
     window.location = homepage
   }
+  document.title = page;
 
   $.ajax({
     url: 'text/' + page + '.md',
