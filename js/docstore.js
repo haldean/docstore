d2ad8ad30b9bd9d5701270d30d06c1b636c1ced5
Will Brown <will.h.brown@gmail.com>
2012-03-14 00:59:36 -0400

    Forward to haldean.org on nonexistent page

--- js/docstore.js
+++ js/docstore.js
@@ -1,14 +1,20 @@
+var homepage = 'http://haldean.org'
+
 $.domReady(function() {
   var page = document.location.search.substring(1)
   if (page[-1] == '/') page = page.substring(0, page.length - 1)
   if (!page) {
-    window.location = 'http://haldean.org'
+    window.location = homepage
   }
 
   $.ajax({
     url: 'text/' + page + '.md',
     type:'html',
     success: function(resp) {
+      if (!resp) {
+        window.location = homepage
+      }
+
       document.getElementById("content").innerHTML = markdown.toHTML(resp)
       $('pre').each(function(el, index) {
         hljs.highlightBlock(el, '  ')
