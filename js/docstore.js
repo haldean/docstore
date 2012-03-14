c7988c11833a60f86150b1b30dadac402549dd35
Will Brown <will.h.brown@gmail.com>
2012-03-14 00:43:55 -0400

    Rookie bug

--- js/docstore.js
+++ js/docstore.js
@@ -1,6 +1,6 @@
 $.domReady(function() {
   var page = document.location.search.substring(1)
-  if (page[-1] = '/') page = page.substring(0, page.length - 1)
+  if (page[-1] == '/') page = page.substring(0, page.length - 1)
   if (!page) {
     window.location = 'http://haldean.org'
   }
